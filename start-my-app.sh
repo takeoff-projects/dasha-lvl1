a8794ec8bf7c6ee1fe70a5870062a70cf7be5de6
dashaTO <dasha@takeoff.com>
2021-09-29 17:09:57 +0300

    more fix

--- start-my-app.sh
+++ start-my-app.sh
@@ -12,10 +12,10 @@ gcloud builds submit --tag gcr.io/$GOOGLE_CLOUD_PROJECT/image10
 cd tf 
 terraform init && terraform apply -auto-approve
 
+cd ../
 gcloud endpoints services deploy openapi-run.yaml --project $GOOGLE_CLOUD_PROJECT
-
 
 gcloud services enable servicemanagement.googleapis.com &&
 gcloud services enable servicecontrol.googleapis.com &&
 gcloud services enable endpoints.googleapis.com &&
-gcloud services enable cloudrun-srv-ega5bq4rma-uc.a.run.app
+gcloud services enable go-pets-ega5bq4rma-uc.a.run.app
